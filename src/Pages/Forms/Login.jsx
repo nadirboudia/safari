ad08a49523d0fedacb6e1c20be1b76abf0931355
nadirboudia <n_boudia@estin.dz>
2025-07-13 02:33:09 +0100

    finished

--- src/Pages/Forms/Login.jsx
+++ src/Pages/Forms/Login.jsx
@@ -1,12 +1,72 @@
-
-import React from 'react'
+import { useEffect, useState } from 'react';
+import './Login.css';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { use } from 'react';
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  
+
+function FormsubmitHandler(e) {
+    e.preventDefault();
+
+    if (email.trim() === '') return toast.error('Email is required');
+    if (password.trim() === '') return toast.error('Password is required');
+
+    const savedUser= JSON.parse(localStorage.getItem("user"))
+
+    //  If same user is already logged in
+    if (
+      savedUser  && savedUser.email === email && savedUser.password ===password
+    ) {
+      return toast.info("You're already logged in.");
+    }
+
+    //  Save new user and show success toast
+    localStorage.setItem("user", JSON.stringify({ email, password }));
+    toast.success("Login successful!");
+  }
+
+
+
+
+
   return (
-    <div>
-      <h1>Form</h1>
+    <div className='form-wrapper'>
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={true}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+      />
+      <form onSubmit={FormsubmitHandler} className='form'>
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          type="email"
+          placeholder='Email'
+        />
+        <input
+          value={password}
+          onChange={(e) => setPassword(e.target.value ) }
+          type="Password"
+          placeholder='Password'
+        />
+        <button onClick={()=>{
+        }}>Login</button>
+        
+       
+      </form>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
